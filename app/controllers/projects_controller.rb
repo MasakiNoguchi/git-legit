201fe31698f1ecfc5709222ce35c20df928b27d8
Giovanni Fantini <fantini.giovanni@gmail.com>
2018-05-22 13:51:40 +0100

    Basic project editing features

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,16 @@
 class ProjectsController < ApplicationController
+
+  def edit
+    @project = Project.find(params[:id])
+    redirect_to edit_project_path(@project)
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+      redirect_to project_path(@project)
+    else
+      render :edit
+    end
+  end
 end
