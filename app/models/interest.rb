42fc8e2606fa89eda9e393d746d2f07176e6772a
Giovanni Fantini <38194756+giovanni-fantini@users.noreply.github.com>
2018-05-23 14:28:46 +0100

    Update interest.rb

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -6,7 +6,4 @@ class Interest < ApplicationRecord
   validates :contribution, presence: true
   validates :user, uniqueness: { scope: :contribution }
 
-  def user_name
-    "#{self.user.first_name} #{self.user.last_name}"
-  end
 end
