ddb696b401ab2562996fcbb5f25071814741f8cc
Sam Jenkins <sam.jenko@hotmail.com>
2018-05-23 17:57:04 +0100

    added interest alert label

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,4 +6,9 @@ class Project < ApplicationRecord
   validates :description, presence: true
   validates :github_url, allow_blank: true, format: { with: /(?:git|ssh|https?|git@[-\w.]+):(\/\/)?(.*?)(\.git)(\/?|\#[-\d\w._]+?)/, message: "Please enter valid GitHub url" }
   validates :user, presence: true
+
+  def interest?
+    array = self.contributions.select { |contribution| contribution.interests.length > 0 && contribution.status == "open" }
+    !array.empty?
+  end
 end
