9f98d1c02fd9420fcf81b911a1d8562b6dec1f36
masaki noguchi <masakinoguchi1@gmail.com>
2018-05-25 14:50:00 +0100

    added photos to project cards

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_24_190939) do
+ActiveRecord::Schema.define(version: 2018_05_25_134637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2018_05_24_190939) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
@@ -91,6 +92,7 @@ ActiveRecord::Schema.define(version: 2018_05_24_190939) do
     t.string "first_name"
     t.string "last_name"
     t.text "bio"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
